8ff02dc073e242e1ff668f0a1974a893410ecc1c
tima101 <dudeness101@gmail.com>
2019-04-19 12:12:58 -0700

    lambda and serverless without logic #18

---
+++ lambda/handler.ts
@@ -0,0 +1,55 @@
+import * as mongoose from 'mongoose';
+
+import { sendEmailNotification } from './src/sendEmailForNewPost';
+
+export const sendEmailForNewPost = async event => {
+  const dev = process.env.NODE_ENV !== 'production';
+
+  const MONGO_URL = dev ? process.env.MONGO_URL_TEST : process.env.MONGO_URL;
+
+  await mongoose.connect(MONGO_URL, { useNewUrlParser: true, useFindAndModify: false });
+
+  try {
+    await sendEmailNotification(process.env.PRODUCTION_URL_APP);
+  } catch (error) {
+    console.error(error.stack);
+
+    return { error: error.message, event };
+  } finally {
+    await mongoose.disconnect();
+  }
+
+  if (!dev && process.env.MONGO_URL2 && process.env.PRODUCTION_URL_APP2) {
+    await mongoose.connect(process.env.MONGO_URL2, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    });
+
+    try {
+      await sendEmailNotification(process.env.PRODUCTION_URL_APP2);
+    } catch (error) {
+      console.error(error.stack);
+
+      return {
+        error: error.message,
+        event,
+      };
+    } finally {
+      await mongoose.disconnect();
+    }
+  }
+
+  return { message: 'Email was sent successfully!', event };
+
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.stringify({
+  //     message: 'Go Serverless v1.0! Your function executed successfully!',
+  //     input: event,
+  //   }),
+  // };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+};
+
+// set up and test API Gateway
---
+++ lambda/src/sendEmailForNewPost.ts
@@ -0,0 +1,19 @@
+// import * as _ from 'lodash';
+// import * as moment from 'moment';
+
+// import sendEmail from './api/aws-ses';
+// import getEmailTemplate, {
+//   EmailTemplate,
+//   IEmailTemplateDocument,
+// } from './api/models/EmailTemplate';
+// import User, { IUserDocument } from './api/models/User';
+
+async function sendEmailNotification(productionUrlApp: string) {
+  console.log(productionUrlApp);
+  // pass more arguments
+  // get email template for new post
+  // find users
+  // send email with passed data to each user with sendEmail()
+}
+
+export { sendEmailNotification };
