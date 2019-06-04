ca96185815af612b201bf8c3820f58d1d56296d5
Pedro Maia Costa <pedro@pmcdigital.pt>
2019-06-04 13:50:13 +0100

    override process.env with dotenv if parsed and fix consts

--- api/server/env.ts
+++ api/server/env.ts
@@ -2,8 +2,14 @@
 // See https://github.com/motdotla/dotenv/tree/master/examples/typescript
 
 import { config } from 'dotenv';
-
 const result = config();
-if (result.error) {
-  throw result.error;
+
+// Only override process.env if .env file is present and valid
+if (!result.error) {
+  Object.keys(result.parsed).forEach(key => {
+    const value = result.parsed[key];
+    if (value && value !== 'undefined') {
+      process.env[key] = value;
+    }
+  });
 }
