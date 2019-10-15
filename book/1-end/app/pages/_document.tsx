7ffeb8c435f909a45cc9ff8bc11fe27d2a042f5d
tima101 <dudeness101@gmail.com>
2019-10-15 13:48:17 -0700

    chapter-1 improvements, TSLint instead of Prettier

--- book/1-end/app/pages/_document.tsx
+++ book/1-end/app/pages/_document.tsx
@@ -1,29 +1,13 @@
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
 
 class MyDocument extends Document {
-  public static getInitialProps = async ctx => {
-
-    const originalRenderPage = ctx.renderPage;
-
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: App => props => <App {...props} />,
-      });
-
-    const initialProps = await Document.getInitialProps(ctx);
-
-    return {
-      ...initialProps,
-    };
-  };
-
   public render() {
     return (
-      <html lang="en">
+      <Html lang="en">
         <Head>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta charSet="utf-8" />{' '}
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />{' '}
           <meta name="google" content="notranslate" />
           <meta name="theme-color" content="#303030" />
         </Head>
@@ -31,7 +15,7 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
