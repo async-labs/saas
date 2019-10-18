e334d6890f304ea081e2770d30b0bff9ea1e5260
tima101 <dudeness101@gmail.com>
2019-10-18 13:17:14 -0700

    settings and rules

--- book/1-end/app/.eslintrc.js
+++ book/1-end/app/.eslintrc.js
@@ -11,4 +11,43 @@ module.exports = {
     jest: true,
   },
   plugins: ['react', 'jsx-a11y', 'import', 'prettier'],
+  rules: {
+    'max-len': ['error', 100],
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    'no-mixed-operators': 'off',
+    'prefer-destructuring': [
+      'error',
+      {
+        VariableDeclarator: {
+          array: false,
+          object: true,
+        },
+        AssignmentExpression: {
+          array: true,
+          object: false,
+        },
+      },
+      {
+        enforceForRenamedProperties: false,
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.js'],
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'all',
+        arrowParens: 'always',
+        printWidth: 100,
+        semi: true,
+      },
+    ],
+  },
 };
