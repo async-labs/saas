1ac6e3a7a8d39ba06169c91dfb962a442904e0aa
Delgermurun Purevkhuu <pdelgermurun@gmail.com>
2020-05-14 14:38:00 +0800

    merge withStore HOC into _app.

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -4,12 +4,13 @@ import { Provider } from 'mobx-react';
 import App from 'next/app';
 import React from 'react';
 
-import { isMobile } from '../lib/isMobile';
-import { Store } from '../lib/store';
 import { themeDark, themeLight } from '../lib/theme';
-import withStore from '../lib/withStore';
+import { getUser } from '../lib/api/public';
+import { getInitialData } from '../lib/api/team-member';
+import { isMobile } from '../lib/isMobile';
+import { getStore, initStore, Store } from '../lib/store';
 
-class MyApp extends App<{ mobxStore: Store; isMobile: boolean }> {
+class MyApp extends App<{ isMobile: boolean }> {
   public static async getInitialProps({ Component, ctx }) {
     const pageProps = { isMobile: isMobile({ req: ctx.req }) };
 
@@ -17,11 +18,46 @@ class MyApp extends App<{ mobxStore: Store; isMobile: boolean }> {
       Object.assign(pageProps, await Component.getInitialProps(ctx));
     }
 
-    return { pageProps };
+    const appProps = { pageProps };
+
+    // if store initialized already, do not load data again
+    if (getStore()) {
+      return appProps;
+    }
+
+    let user = null;
+    try {
+      user = ctx.req ? ctx.req.user : await getUser();
+    } catch (error) {
+      console.log(error);
+    }
+
+    let initialData = {};
+    const { teamSlug, discussionSlug } = ctx.query;
+
+    if (user) {
+      try {
+        initialData = await getInitialData({
+          request: ctx.req,
+          data: { teamSlug, discussionSlug },
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    return {
+      ...appProps,
+      initialState: { user, teamSlug, currentUrl: ctx.asPath, ...initialData },
+    };
   }
+
+  private store: Store;
 
   constructor(props) {
     super(props);
+
+    this.store = initStore(props.initialState);
   }
 
   public componentDidMount() {
@@ -33,14 +69,15 @@ class MyApp extends App<{ mobxStore: Store; isMobile: boolean }> {
   }
 
   public render() {
-    const { Component, pageProps, mobxStore } = this.props;
+    const { Component, pageProps } = this.props;
+    const store = this.store;
 
     return (
       <ThemeProvider
-        theme={mobxStore.currentUser && mobxStore.currentUser.darkTheme ? themeDark : themeLight}
+        theme={store.currentUser && store.currentUser.darkTheme ? themeDark : themeLight}
       >
         <CssBaseline />
-        <Provider store={mobxStore}>
+        <Provider store={store}>
           <Component {...pageProps} />
         </Provider>
       </ThemeProvider>
@@ -48,4 +85,4 @@ class MyApp extends App<{ mobxStore: Store; isMobile: boolean }> {
   }
 }
 
-export default withStore(MyApp);
+export default MyApp;
