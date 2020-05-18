de64b1ad9a9d7e4a3869270937ab92660a2387f7
tima101 <dudeness101@gmail.com>
2020-05-18 13:46:52 -0700

    book/7-end no need for new HOC

--- book/7-end/app/pages/_app.tsx
+++ book/7-end/app/pages/_app.tsx
@@ -3,12 +3,14 @@ import { ThemeProvider } from '@material-ui/styles';
 import { Provider } from 'mobx-react';
 import App from 'next/app';
 import React from 'react';
-import { isMobile } from '../lib/isMobile';
-import { Store } from '../lib/store';
-import { themeDark, themeLight } from '../lib/theme';
-import withStore from '../lib/withStore';
 
-class MyApp extends App<{ isMobile: boolean; mobxStore: Store }> {
+import { themeDark, themeLight } from '../lib/theme';
+import { getUserApiMethod } from '../lib/api/public';
+// import { getInitialDataApiMethod } from '../lib/api/team-member';
+import { isMobile } from '../lib/isMobile';
+import { getStore, initStore, Store } from '../lib/store';
+
+class MyApp extends App<{ isMobile: boolean }> {
   public static async getInitialProps({ Component, ctx }) {
     let firstGridItem = true;
 
@@ -22,7 +24,38 @@ class MyApp extends App<{ isMobile: boolean; mobxStore: Store }> {
       Object.assign(pageProps, await Component.getInitialProps(ctx));
     }
 
-    return { pageProps };
+    const appProps = { pageProps };
+
+    // if store initialized already, do not load data again
+    if (getStore()) {
+      return appProps;
+    }
+
+    let user = null;
+    try {
+      user = ctx.req ? ctx.req.user : await getUserApiMethod();
+    } catch (error) {
+      console.log(error);
+    }
+
+    // let initialData = {};
+    // const { teamSlug, discussionSlug } = ctx.query;
+
+    // if (user) {
+    //   try {
+    //     initialData = await getInitialDataApiMethod({
+    //       request: ctx.req,
+    //       data: { teamSlug, discussionSlug },
+    //     });
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // }
+
+    return {
+      ...appProps,
+      initialState: { user, currentUrl: ctx.asPath },
+    };
   }
 
   public componentDidMount() {
@@ -32,15 +65,25 @@ class MyApp extends App<{ isMobile: boolean; mobxStore: Store }> {
       jssStyles.parentNode.removeChild(jssStyles);
     }
   }
+
+  private store: Store;
+
+  constructor(props) {
+    super(props);
+
+    this.store = initStore(props.initialState);
+  }
+
   public render() {
-    const { Component, pageProps, mobxStore } = this.props;
+    const { Component, pageProps } = this.props;
+    const store = this.store;
 
     return (
       <ThemeProvider
-        theme={mobxStore.currentUser && mobxStore.currentUser.darkTheme ? themeDark : themeLight}
+        theme={store.currentUser && store.currentUser.darkTheme ? themeDark : themeLight}
       >
         <CssBaseline />
-        <Provider store={mobxStore}>
+        <Provider store={store}>
           <Component {...pageProps} />
         </Provider>
       </ThemeProvider>
@@ -48,4 +91,4 @@ class MyApp extends App<{ isMobile: boolean; mobxStore: Store }> {
   }
 }
 
-export default withStore(MyApp);
+export default MyApp;
