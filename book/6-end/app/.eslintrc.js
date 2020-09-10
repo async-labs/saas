4ec8565682120174ba28417e322f9dfa28e082be
tima101 <dudeness101@gmail.com>
2020-09-10 10:57:59 -0700

    .eslintrc.js

--- book/6-end/app/.eslintrc.js
+++ book/6-end/app/.eslintrc.js
@@ -1,34 +1,29 @@
 module.exports = {
   settings: {
-    react: { version: "detect" }
+    react: { version: 'detect' },
   },
-  parser: "@typescript-eslint/parser",
-  extends: [
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier"
-  ],
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   env: {
     es6: true,
-    node: true
+    node: true,
   },
   rules: {
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
         singleQuote: true,
-        trailingComma: "all",
-        arrowParens: "always",
+        trailingComma: 'all',
+        arrowParens: 'always',
         printWidth: 100,
-        semi: true
-      }
+        semi: true,
+      },
     ],
-    "@typescript-eslint/camelcase": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "react/no-unescaped-entities": "off",
-    "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
-    "@typescript-eslint/no-explicit-any": "off",
- },
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+    '@typescript-eslint/no-explicit-any': 'off',
   },
-  plugins: ["prettier", "react"]
+  plugins: ['prettier', 'react'],
 };
