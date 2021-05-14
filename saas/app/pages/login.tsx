5e9f9d3992fed1fec203db41f87325ab656ff4b6
tima101 <kellyburke101@gmail.com>
2021-05-14 05:09:16 -0700

    add note to login page

--- saas/app/pages/login.tsx
+++ saas/app/pages/login.tsx
@@ -22,8 +22,9 @@ class Login extends React.Component {
           </p>
           <p>You’ll be logged in for 14 days unless you log out manually.</p>
           <br />
-
           <LoginButton />
+          <br />
+          <p>Using a fake email address will not allow you to log in to the demo.</p>
         </div>
       </Layout>
     );
